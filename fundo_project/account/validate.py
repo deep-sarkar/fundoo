17564927a40216f9fdea2c4340b03965684c2eb8
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 18:43:16 +0530

    [Deep]Refactor.modified coading space

--- fundo_project/account/validate.py
+++ fundo_project/account/validate.py
@@ -1,6 +1,13 @@
-from .exceptions import PasswordDidntMatched
+from .exceptions import PasswordDidntMatched, PasswordPatternMatchError
 from .status import response_code
+
+#Regex
+import re
 
 def validate_password_match(password1,password2):
     if password1 != password2:
-        raise PasswordDidntMatched(code =403,msg=response_code[403])
+        raise PasswordDidntMatched(code=403,msg=response_code[403])
+
+def validate_password_pattern_match(password):
+    if not re.search('^[a-zA-Z0-9]{8,}$',password):
+        raise PasswordPatternMatchError(code=406,msg=response_code[406])
