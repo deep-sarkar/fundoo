357fe5dea180daefdb42d2fe5110ce2d6eee1a06
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 18:01:46 +0530

    [Deep]Refactor.modified exception message and codes

---
+++ fundo_project/account/exceptions.py
@@ -0,0 +1,14 @@
+class UsernameAlreadyExistsError(Exception):
+    def __init__(self,msg,code):
+        self.code = code
+        self.msg = msg
+
+class EmailAlreadyExistsError(Exception):
+    def __init__(self,msg,code):
+        self.code = code
+        self.msg = msg
+
+class PasswordDidntMatched(Exception):
+    def __init__(self,msg,code):
+        self.code = code
+        self.msg = msg
