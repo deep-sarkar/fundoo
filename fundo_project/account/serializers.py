1e7de2cd36a2c76e2972a1c2ac98be0a01c63ba6
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 19:29:45 +0530

    [Deep]Refactor.removed un necessory codes

--- fundo_project/account/serializers.py
+++ fundo_project/account/serializers.py
@@ -22,27 +22,3 @@ class RegistrationSerializer(serializers.ModelSerializer):
             ]
         extra_kwargs = {'password':{'write_only':True}}
         required_fields = ['username','email','password','confirm_password']
-
-    # def validate(self,data):
-    #     password = data.get('password')
-    #     confirm_password = data.get('confirm_password')
-    #     if password != confirm_password:
-    #         raise PasswordDidntMatched(response_code[403])
-    #     return data
-        
-    # def create(self,validated_data):
-    #     user_obj = User(first_name=validated_data.get('first_name'),
-    #                     last_name=validated_data.get('last_name'),
-    #                     username=validated_data.get('username'), 
-    #                     email = validated_data.get('email'))
-    #     user_obj.set_password(validated_data.get('password'))
-    #     user_obj.active = False
-    #     user_obj.save()
-    #     return user_obj
-        
-
-  
-
-
-
-    
