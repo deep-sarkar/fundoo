d164086de14c0b7dfbfe530c0b2a7d93a8c1ad1a
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 18:14:52 +0530

    [Deep]Refactor.modified method name to validate_password_match

--- fundo_project/account/validate.py
+++ fundo_project/account/validate.py
@@ -1,6 +1,6 @@
 from .exceptions import PasswordDidntMatched
 from .status import response_code
 
-def validate_password(password1,password2):
+def validate_password_match(password1,password2):
     if password1 != password2:
         raise PasswordDidntMatched(code =403,msg=response_code[403])
