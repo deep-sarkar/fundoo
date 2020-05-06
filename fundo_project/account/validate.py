facfcd9a07439c1da9971c55627a743990e3a564
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 19:31:51 +0530

    [Deep]Add.added validation for existing user

--- fundo_project/account/validate.py
+++ fundo_project/account/validate.py
@@ -1,5 +1,10 @@
-from .exceptions import PasswordDidntMatched, PasswordPatternMatchError
+from .exceptions import PasswordDidntMatched, PasswordPatternMatchError, UsernameAlreadyExistsError
 from .status import response_code
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 #Regex
 import re
@@ -11,3 +16,7 @@ def validate_password_match(password1,password2):
 def validate_password_pattern_match(password):
     if not re.search('^[a-zA-Z0-9]{8,}$',password):
         raise PasswordPatternMatchError(code=406,msg=response_code[406])
+
+def validate_username_existance(username):
+    if User.objects.filter(username=username).exists():
+        raise UsernameAlreadyExistsError(code=407,msg=response_code[407])
