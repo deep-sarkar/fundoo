e5b59a0da56b36b99f87dc9792e71685051f3720
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 17:59:21 +0530

    [Deep]Refactor.removed unwanted codes

--- fundo_project/account/serializers.py
+++ fundo_project/account/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .status import USERNAME_ALREADY_EXISTS, PASSWORD_DIDNT_MATCHED, EMAIL_ID_ALREAADY_EXISTS
+from .exceptions import PasswordDidntMatched
+from .status import response_code
 
 
 
@@ -22,26 +23,25 @@ class RegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password':{'write_only':True}}
         required_fields = ['username','email','password','confirm_password']
 
-    def validate_username(self, data):
-        quaryset = User.objects.filter(username__iexact=data)
-        if quaryset.count() != 0:
-            raise serializers.ValidationError(USERNAME_ALREADY_EXISTS)
-        return data
-    
-    def validate(self,data):
-        pw = data.get('password')
-        pw2 = data.get('confirm_password')
-        if pw != pw2:
-            raise serializers.ValidationError(PASSWORD_DIDNT_MATCHED)
-        return data
+    # def validate(self,data):
+    #     password = data.get('password')
+    #     confirm_password = data.get('confirm_password')
+    #     if password != confirm_password:
+    #         raise PasswordDidntMatched(response_code[403])
+    #     return data
+        
+    # def create(self,validated_data):
+    #     user_obj = User(first_name=validated_data.get('first_name'),
+    #                     last_name=validated_data.get('last_name'),
+    #                     username=validated_data.get('username'), 
+    #                     email = validated_data.get('email'))
+    #     user_obj.set_password(validated_data.get('password'))
+    #     user_obj.active = False
+    #     user_obj.save()
+    #     return user_obj
+        
 
-    def validate_email(self, value):
-        qs = User.objects.filter(email=value)
-        if qs.exists():
-            raise serializers.ValidationError(EMAIL_ID_ALREAADY_EXISTS)
-        return value
-
-
+  
 
 
 
