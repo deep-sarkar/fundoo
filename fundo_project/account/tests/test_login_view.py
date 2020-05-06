ea0cd451127a5de8fa891e3838f3f226744de641
deep-sarkar <deepsarkar96@gmail.com>
2020-05-06 23:19:46 +0530

    [Deep]Refactor.modified user login test with unauthorised user

--- fundo_project/account/tests/test_login_view.py
+++ fundo_project/account/tests/test_login_view.py
@@ -1,19 +1,24 @@
 import pytest
 from .. import views
 from django.test import RequestFactory
+from django.contrib.auth import get_user_model
 
-pytestmark = pytest.mark.django_backend
+pytestmark = pytest.mark.django_db
 
+User = get_user_model()
 
+@pytest.mark.django_db
 class TestLoginView:
 
-    username = "tintin"
-    email    = "tintin@gmail.com"
-    password = "tintin11"
-
-    def test_if_user_doesnt_exist_returns_409(self):
-        username = self.username
-        request   = RequestFactory().post('/',username)
+    def test_if_user_doesnt_exist_returns_409(self,db):
+        user = User.objects.create(first_name ='tintin', last_name='tintin', username='tintin',
+                                    email='tintin@gmail.com')
+        user.set_password('password')
+        detail = {
+            'username':'test',
+            'password':'password'
+        }
+        request   = RequestFactory().post('/',detail)
         response  = views.LoginAPIView.as_view()(request)
         assert response.data['code'] == 409
 
