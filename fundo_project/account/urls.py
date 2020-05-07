784793112fd804c9e0f29266135c0fa549cf438c
deep-sarkar <deepsarkar96@gmail.com>
2020-05-07 08:49:23 +0530

    [Deep]Add.added path for login:

--- fundo_project/account/urls.py
+++ fundo_project/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Registration
+from .views import Registration, LoginAPIView
 
 urlpatterns = [
-    path('register/', Registration.as_view(), name='registration')
+    path('register/', Registration.as_view(), name='registration'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
