02ac23ef467d0746661172c7b3edfea63e52196e
deep-sarkar <deepsarkar96@gmail.com>
2020-05-07 20:43:45 +0530

    [Deep]Refactor.modified setUp defination

--- fundo_project/account/tests/test_login_view.py
+++ fundo_project/account/tests/test_login_view.py
@@ -10,10 +10,12 @@ User = get_user_model()
 @pytest.mark.django_db
 class TestLoginView:
 
-    def test_if_user_doesnt_exist_returns_409(self,db):
-        user = User.objects.create(first_name ='tintin', last_name='tintin', username='tintin',
-                                    email='tintin@gmail.com')
-        user.set_password('password')
+    @pytest.fixture
+    def setUp(self):
+        User.objects.create_user(username='tintin', password='tintin123')
+
+
+    def test_if_user_doesnt_exist_returns_409(self, setUp):
         detail = {
             'username':'test',
             'password':'password'
@@ -22,10 +24,7 @@ class TestLoginView:
         response  = views.LoginAPIView.as_view()(request)
         assert response.data['code'] == 409
     
-    def test_authentication_without_password_returns_406(self):
-        user = User.objects.create(first_name ='tintin', last_name='tintin', username='tintin',
-                                    email='tintin@gmail.com')
-        user.set_password('password')
+    def test_authentication_without_password_returns_406(self, setUp):
         detail = {
             'username':'tintin',
             'password':''
@@ -34,16 +33,14 @@ class TestLoginView:
         response  = views.LoginAPIView.as_view()(request)
         assert response.data['code'] == 406
     
-    def test_authentication_invalidated_password_returns_412(self):
-        user = User.objects.create(first_name ='tintin', last_name='tintin', username='tintin',
-                                    email='tintin@gmail.com')
-        user.is_active = True
-        user.set_password("tintin123")
+    def test_authentication_invalidated_password_returns_412(self, setUp):
         detail = {
             "username":"tintin",
             "password":"tintin12"
             }   
-        request   = RequestFactory().post('/account/login/',detail)
+        request   = RequestFactory().post('/',detail)
         response  = views.LoginAPIView.as_view()(request)
         assert response.data['code'] == 412
+        
+
 
