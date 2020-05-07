cf3de4fd1b59bd42c0328134546b61c66706e379
deep-sarkar <deepsarkar96@gmail.com>
2020-05-07 09:09:45 +0530

    [Deep]Add.added logout url path:

--- fundo_project/account/urls.py
+++ fundo_project/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Registration, LoginAPIView
+from .views import Registration, LoginAPIView, Logout
 
 urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
