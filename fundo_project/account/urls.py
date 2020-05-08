e9498cc86c156570b5723c6eccecb657797afca6
deep-sarkar <deepsarkar96@gmail.com>
2020-05-08 09:54:27 +0530

    [Deep]Add.added url path for ResetPasswordView

--- fundo_project/account/urls.py
+++ fundo_project/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Registration, LoginAPIView, Logout
+from .views import Registration, LoginAPIView, Logout, ResetPasswordView
 
 urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
+    path('reset_password/',ResetPasswordView.as_view(), name='reset_password'),
 ]
