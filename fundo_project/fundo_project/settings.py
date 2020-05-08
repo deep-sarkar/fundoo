ccdd3d6b8174e5a5fffb7f22bd83f68a555c6228
deep-sarkar <deepsarkar96@gmail.com>
2020-05-08 11:09:46 +0530

    [Deep]Add.added SMTP settings

--- fundo_project/fundo_project/settings.py
+++ fundo_project/fundo_project/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#For email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wotbsingh@gmail.com'
+EMAIL_HOST_PASSWORD = '9950618403'
+EMAIL_USE_SSL = False
