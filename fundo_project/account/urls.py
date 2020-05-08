654042138e6977501393a9232206d138505469a8
deep-sarkar <deepsarkar96@gmail.com>
2020-05-08 20:51:14 +0530

    [Deep]Add.added activate account path

--- fundo_project/account/urls.py
+++ fundo_project/account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Registration, LoginAPIView, Logout, ResetPasswordView
+from .views import Registration, LoginAPIView, Logout, ResetPasswordView, ActivateAccount
 
 urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
     path('reset_password/',ResetPasswordView.as_view(), name='reset_password'),
+    path('activate/<surl>/', ActivateAccount.as_view(), name='activate'),
 ]
