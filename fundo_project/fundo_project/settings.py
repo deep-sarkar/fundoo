ed2c6f2ab8c7549e370ad2c174c5e63ad6d7a565
deep-sarkar <deepsarkar96@gmail.com>
2020-05-08 19:54:09 +0530

    [Deep]Add.added static directory

--- fundo_project/fundo_project/settings.py
+++ fundo_project/fundo_project/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 #For email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
