5b708407e252697bc2c99f5e70eb305233be1d17
deep-sarkar <deepsarkar96@gmail.com>
2020-05-12 11:15:49 +0530

    [Deep]Add.added forgot password Endpoint

--- fundo_project/account/urls.py
+++ fundo_project/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Registration, LoginAPIView, Logout, ResetPasswordView, ActivateAccount
+from .views import Registration, LoginAPIView, Logout, ResetPasswordView, ActivateAccount, ForgotPasswordView
 
 urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', Logout.as_view(), name='logout'),
     path('reset_password/',ResetPasswordView.as_view(), name='reset_password'),
     path('activate/<surl>/', ActivateAccount.as_view(), name='activate'),
+    path('forgot_password/',ForgotPasswordView.as_view(), name='forgot_password'),
 ]
