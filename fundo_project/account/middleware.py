f94dd3799e834d13bd7d7d2f6fd3489cf9908b7a
deep-sarkar <deepsarkar96@gmail.com>
2020-07-17 11:14:04 +0530

    [Deep]Add.added custom middleware LoginRequiredMiddleware

---
+++ fundo_project/account/middleware.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+import re
+from django.shortcuts import redirect
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+    
