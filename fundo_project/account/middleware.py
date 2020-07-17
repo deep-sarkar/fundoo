6df312ae51e6f87b3413132809903a5b7093e181
deep-sarkar <deepsarkar96@gmail.com>
2020-07-17 11:16:10 +0530

    [Deep]Add.added process_view method to validate LoginRequiredMiddleware method

--- fundo_project/account/middleware.py
+++ fundo_project/account/middleware.py
@@ -2,6 +2,12 @@ from django.conf import settings
 import re
 from django.shortcuts import redirect
 
+LOGIN_REQUIRED_URLS = [re.compile(settings.LOGOUT_URL.lstrip('/'))]
+
+if hasattr(settings, 'LOGIN_REQUIRED_URLS'):
+    LOGIN_REQUIRED_URLS += [re.compile(url) for url in settings.LOGIN_REQUIRED_URLS]
+
+
 class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -10,3 +16,11 @@ class LoginRequiredMiddleware:
         response = self.get_response(request)
         return response
     
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        assert hasattr(request, 'user')
+        path = request.path_info.lstrip('/')
+        print(path)
+
+        if not request.user.is_authenticated:
+            if any(url.match(path) for url in LOGIN_REQUIRED_URLS):
+                return redirect(settings.LOGIN_URL)
