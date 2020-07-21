833bea8daa7e913d0463e17a6faaa0f4e1d4c94a
deep-sarkar <deepsarkar96@gmail.com>
2020-07-21 11:16:09 +0530

    [Deep]Add.added display notes url

--- fundo_project/notes/urls.py
+++ fundo_project/notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateNoteView
+from .views import CreateNoteView, DisplayNotesView
 
 urlpatterns = [
     path('create/', CreateNoteView.as_view(), name='create-note'),
+    path('display/', DisplayNotesView.as_view(), name='dsiplay-notes'),
 
 ]
