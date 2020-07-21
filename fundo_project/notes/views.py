c55ce8ba54d351f6e78d53969d99656e44bf76c8
deep-sarkar <deepsarkar96@gmail.com>
2020-07-21 09:51:14 +0530

    [Deep]Add.addded CreateNotes view

--- fundo_project/notes/views.py
+++ fundo_project/notes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+from .models import Note
+from .serializers import NoteSerializer
+from account.status import response_code
 
-# Create your views here.
+class CreateNotes(GenericAPIView):
+    serializer_class = NoteSerializer
+    
+    def post(self, request):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user_id=self.request.user.id)
+            return Response({'code':201,'msg':response_code[201]})
+        return Response({'code':405,'msg':response_code[405]})
+        
