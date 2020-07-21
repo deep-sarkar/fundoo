1108f7c4793d4169b0d614c30f483230769c846d
deep-sarkar <deepsarkar96@gmail.com>
2020-07-21 12:01:56 +0530

    [Deep]Add.added put/updtae method for single note

--- fundo_project/notes/views.py
+++ fundo_project/notes/views.py
@@ -23,6 +23,7 @@ class CreateNoteView(GenericAPIView):
         return Response({'code':405,'msg':response_code[405]})
 
 class DisplayNoteView(GenericAPIView):
+    serializer_class = NoteSerializer
 
     def get_object(self,id):
         try:
@@ -33,4 +34,14 @@ class DisplayNoteView(GenericAPIView):
     def get(self, request, id=None):
         note = self.get_object(id)
         serializer = NoteSerializer(note)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
+    
+    def put(self, request, id=None):
+        data       = request.data
+        note       = self.get_object(id)
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save(user_id=self.request.user.id)
+            return Response(serializer.data, status=200)
+        return Response({'code':405,'msg':response_code[405]})
+
