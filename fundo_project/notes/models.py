a8508c7c47a31da0d4514659868c3b46f1aa8bdb
deep-sarkar <deepsarkar96@gmail.com>
2020-07-21 09:49:15 +0530

    [Deep]Add.added Label and Note models

--- fundo_project/notes/models.py
+++ fundo_project/notes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Label(models.Model):
+    label_id    = models.ForeignKey(User, on_delete=models.CASCADE)
+    label       = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.label
+    
+class Note(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=25, blank=False , null=False)
+    note  = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to = 'static/images',max_length=255, null=True, blank=True)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
