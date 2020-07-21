fcc5cdf3c61d2d2bc7acedb7ef17a63a070fa9c9
deep-sarkar <deepsarkar96@gmail.com>
2020-07-21 11:31:44 +0530

    [Deep]Refactor.modified user field to read only attribute

--- fundo_project/notes/serializers.py
+++ fundo_project/notes/serializers.py
@@ -2,18 +2,18 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Label, Note
 
 class LabelSerializer(ModelSerializer):
-    user = SerializerMethodField('_user')
+    user = SerializerMethodField('_user',read_only=True)
     def _user(self, obj):
         request = getattr(self.context, 'request', None)
         if request:
             return request.user
-            
+
     class Meta:
         model = Label
         fields = ['user','label']
 
 class NoteSerializer(ModelSerializer):
-    user = SerializerMethodField('_user')
+    user = SerializerMethodField('_user',read_only=True)
     def _user(self, obj):
         request = getattr(self.context, 'request', None)
         if request:
