552391998c8f3e3b5124404c12798c76a1e75914
deep-sarkar <deepsarkar96@gmail.com>
2020-07-22 10:15:22 +0530

    [Deep]Add.added CreateLabelView into urls

--- fundo_project/notes/urls.py
+++ fundo_project/notes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateNoteView, DisplayNoteView
+from .views import CreateNoteView, DisplayNoteView, CreateLabelView
 
 urlpatterns = [
     path('create/', CreateNoteView.as_view(), name='create-note'),
     path('display/<int:id>/', DisplayNoteView.as_view(), name='dsiplay-notes'),
+    path('create-label/', CreateLabelView.as_view(), name='create-label'),
 
 ]
