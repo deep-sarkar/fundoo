00c95445b2d33fc0c50cf5d845167fc780c4405d
deep-sarkar <deepsarkar96@gmail.com>
2020-07-22 15:59:27 +0530

    [Deep]Refactor.modified default_detal for DoesNotExistsException

--- fundo_project/notes/exceptions.py
+++ fundo_project/notes/exceptions.py
@@ -2,5 +2,5 @@ from rest_framework.exceptions import APIException
 
 class DoesNotExistException(APIException):
     status_code = 409
-    default_detail = 'Note does not exists'
+    default_detail = 'does not exists'
     default_code = 'DOES NOT EXISTS'
