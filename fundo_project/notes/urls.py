7dcb44fd5f62ccb95abc48e6ae0ecfa35cbdffdb
deep-sarkar <deepsarkar96@gmail.com>
2020-07-22 15:14:40 +0530

    [Deep]Add.added display label by id url

--- fundo_project/notes/urls.py
+++ fundo_project/notes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CreateNoteView, DisplayNoteView, CreateLabelView
+from .views import CreateNoteView, DisplayNoteView, CreateLabelView, DisplayLabelView
 
 urlpatterns = [
     path('create/', CreateNoteView.as_view(), name='create-note'),
     path('display/<int:id>/', DisplayNoteView.as_view(), name='dsiplay-notes'),
     path('create-label/', CreateLabelView.as_view(), name='create-label'),
-
+    path('display-label/<int:id>/', DisplayLabelView.as_view(), name='display-label'),
 ]
