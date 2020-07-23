cc42aa9f6794bf953690b5347e088b59a6e64556
deep-sarkar <deepsarkar96@gmail.com>
2020-07-23 19:57:13 +0530

    [Deep]Add.added url for swagger

--- fundo_project/fundo_project/urls.py
+++ fundo_project/fundo_project/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='FUNDOO API DOCUMENTATION')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls'), name='accounts'),
     path('social-auth/', include('social_django.urls', namespace='social')),
-    path('notes/', include('notes.urls'), name='notes')
+    path('notes/', include('notes.urls'), name='notes'),
+    path('', schema_view)
 ]
