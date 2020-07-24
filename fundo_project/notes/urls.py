393f718f5be64c9c1f525e2fa0b7b7c4f107f77f
deep-sarkar <deepsarkar96@gmail.com>
2020-07-24 15:54:10 +0530

    [Deep]Add.added url to access single trash object

--- fundo_project/notes/urls.py
+++ fundo_project/notes/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import CreateNoteView, DisplayNoteView, CreateLabelView, DisplayLabelView, AllTrashedNotesView
+from .views import (CreateNoteView, 
+                    DisplayNoteView, 
+                    CreateLabelView, 
+                    DisplayLabelView, 
+                    AllTrashedNotesView, 
+                    TrashNoteView)
+
 
 urlpatterns = [
     path('create/', CreateNoteView.as_view(), name='create-note'),
@@ -7,4 +13,5 @@ urlpatterns = [
     path('create-label/', CreateLabelView.as_view(), name='create-label'),
     path('display-label/<int:id>/', DisplayLabelView.as_view(), name='display-label'),
     path('trash/',AllTrashedNotesView.as_view(), name='all-trashed-notes'),
+    path('trash/<int:id>/', TrashNoteView.as_view(), name='single-trash-note'),
 ]
