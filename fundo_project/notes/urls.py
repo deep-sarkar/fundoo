95688024518a3690d850e2282870a5d579d1f52c
deep-sarkar <deepsarkar96@gmail.com>
2020-07-24 15:40:12 +0530

    [Deep]Add.added AllTrashedNoteView url

--- fundo_project/notes/urls.py
+++ fundo_project/notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateNoteView, DisplayNoteView, CreateLabelView, DisplayLabelView
+from .views import CreateNoteView, DisplayNoteView, CreateLabelView, DisplayLabelView, AllTrashedNotesView
 
 urlpatterns = [
     path('create/', CreateNoteView.as_view(), name='create-note'),
     path('display/<int:id>/', DisplayNoteView.as_view(), name='dsiplay-notes'),
     path('create-label/', CreateLabelView.as_view(), name='create-label'),
     path('display-label/<int:id>/', DisplayLabelView.as_view(), name='display-label'),
+    path('trash/',AllTrashedNotesView.as_view(), name='all-trashed-notes'),
 ]
