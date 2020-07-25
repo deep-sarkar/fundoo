2e0ab58b4629b2bd43f69c9b14f9d7db52acabef
deep-sarkar <deepsarkar96@gmail.com>
2020-07-25 11:36:18 +0530

    [Deep]Refactor.modified label as ManyToManyField

--- fundo_project/notes/models.py
+++ fundo_project/notes/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Label(models.Model):
-    label_id    = models.ForeignKey(User, on_delete=models.CASCADE)
+    label_id    = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     label       = models.CharField(max_length=30, null=False, blank=False)
 
     def __str__(self):
         return self.label
     
 class Note(models.Model):
-    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     title = models.CharField(max_length=25, blank=False , null=False)
     note  = models.TextField(blank=True,null=True)
     image = models.ImageField(upload_to = 'static/images',max_length=255, null=True, blank=True)
-    label = models.ForeignKey(Label, on_delete=models.CASCADE, null=True, blank=True)
+    label = models.ManyToManyField(Label, blank=True)
     trash = models.BooleanField(default=False)
 
     def __str__(self):
