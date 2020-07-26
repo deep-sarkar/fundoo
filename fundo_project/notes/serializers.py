57d98ad27ce14224a128d67fb59cfd4518100729
deep-sarkar <deepsarkar96@gmail.com>
2020-07-26 15:47:24 +0530

    [Deep]Add.added update method in SingleNoteSerializer

--- fundo_project/notes/serializers.py
+++ fundo_project/notes/serializers.py
@@ -23,10 +23,29 @@ class NoteSerializer(serializers.ModelSerializer):
         return note
 
 class SingleNoteSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model            = Note
         fields           = '__all__'
         read_only_fields = ['id','user']
+
+    def update(self, instance, validated_data):
+        user      = validated_data['user']
+        label     = validated_data['label']
+        instance.title     = validated_data.get("title", instance.title)
+        instance.note      =validated_data.get("note", instance.note)
+        instance.urls      =validated_data.get("urls", instance.urls)
+        instance.image     =validated_data.get("image", instance.image)
+        instance.reminder  =validated_data.get("reminder", instance.reminder)
+        instance.archives  =validated_data.get("archives", instance.archives)
+        instance.trash     =validated_data.get("trash", instance.trash)
+        instance.pin       =validated_data.get("pin", instance.pin)
+        instance.label     = validated_data.get("label", instance.label)
+        labels = label.split(',')
+        for label in labels:
+            Label.objects.get_or_create(label_id=user, label=label)
+        instance.save()
+        return instance
 
 class TrashSerializer(serializers.ModelSerializer):
     class Meta:
