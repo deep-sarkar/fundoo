28f8ea53a03762d6df62356a4a1617131d276caa
deep-sarkar <deepsarkar96@gmail.com>
2020-07-26 11:33:06 +0530

    [Deep]Add.added pin, url and archive field in Note model

--- fundo_project/notes/models.py
+++ fundo_project/notes/models.py
@@ -14,10 +14,16 @@ class Note(models.Model):
     user      = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     title     = models.CharField(max_length=25, blank=False , null=False)
     note      = models.TextField(blank=True,null=True)
+    urls      = models.URLField(blank=True, null=True)
     image     = models.ImageField(upload_to = 'static/images',max_length=255, null=True, blank=True)
-    label     = models.ManyToManyField(Label, blank=True)
-    trash     = models.BooleanField(default=False)
     reminder  = models.DateTimeField(auto_now_add=False, null=True)
+    archives  = models.BooleanField(default=False)
+    trash     = models.BooleanField(default=False)
+    pin       = models.BooleanField(default=False)
+    label     = models.CharField(max_length=120,blank=True, null=True)
+
+    class Meta:
+        ordering=['-id']
 
     def __str__(self):
         return self.title
