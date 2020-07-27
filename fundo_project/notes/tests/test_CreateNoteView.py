4e9db13eb3e76929bb65ec163bd40efb916207fc
deep-sarkar <deepsarkar96@gmail.com>
2020-07-27 11:21:21 +0530

    [Deep]Add.added test for get method

--- fundo_project/notes/tests/test_CreateNoteView.py
+++ fundo_project/notes/tests/test_CreateNoteView.py
@@ -2,7 +2,10 @@ import pytest
 from ..views import CreateNoteView
 from django.test import RequestFactory
 
-# pytestmark = pytest.mark.django_db
+import mock
+import os
+
+pytestmark = pytest.mark.django_db
 
 class TestCreateNoteView:
 
@@ -11,3 +14,20 @@ class TestCreateNoteView:
         request  = RequestFactory().post('/',data)
         response = CreateNoteView.as_view()(request)
         assert response.data['code'] == 405
+
+
+    def test_get_method_returns_200(self):
+        request  = RequestFactory().get('/')
+        response = CreateNoteView.as_view()(request)
+        assert response.status_code == 200
+
+    # @mock.patch('notes.views.CreateNoteView.post.user', mock.MagicMock(return_value=1))
+    # def test_post_method_with_valid_data_dictionary_will_return_201(self):
+    #     data = {
+    #         'title':'hello',
+    #         'label':'',
+    #     }
+    #     request  = RequestFactory().post('/',data)
+    #     response = CreateNoteView.as_view()(request)
+    #     assert response.data['code'] == 201
+    
