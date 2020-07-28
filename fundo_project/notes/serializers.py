73086ebf885e48c7d5c7294af6c60f36eab09ebb
deep-sarkar <deepsarkar96@gmail.com>
2020-07-28 11:38:26 +0530

    [Deep]Refactor.modified label field as SerializerList field

--- fundo_project/notes/serializers.py
+++ fundo_project/notes/serializers.py
@@ -15,36 +15,32 @@ class NoteSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user  = validated_data['user']
-        label = validated_data['label']
+        labels = validated_data['label']
         note  = Note.objects.create(**validated_data)
-        labels = label.split(',')
         for label in labels:
-            label = label.strip()
             Label.objects.get_or_create(label_id=user, label=label)
         return note
 
 class SingleNoteSerializer(serializers.ModelSerializer):
-    
+    label = serializers.ListField(child=serializers.CharField())
     class Meta:
         model            = Note
-        fields           = '__all__'
+        fields           = ['user','title','note','urls','image','reminder','archives','trash','pin','label']
         read_only_fields = ['id','user']
 
     def update(self, instance, validated_data):
-        user      = validated_data['user']
-        label     = validated_data['label']
+        user       = validated_data['user']
+        labels     = validated_data['label']
         instance.title     = validated_data.get("title", instance.title)
-        instance.note      =validated_data.get("note", instance.note)
-        instance.urls      =validated_data.get("urls", instance.urls)
-        instance.image     =validated_data.get("image", instance.image)
-        instance.reminder  =validated_data.get("reminder", instance.reminder)
-        instance.archives  =validated_data.get("archives", instance.archives)
-        instance.trash     =validated_data.get("trash", instance.trash)
-        instance.pin       =validated_data.get("pin", instance.pin)
+        instance.note      = validated_data.get("note", instance.note)
+        instance.urls      = validated_data.get("urls", instance.urls)
+        instance.image     = validated_data.get("image", instance.image)
+        instance.reminder  = validated_data.get("reminder", instance.reminder)
+        instance.archives  = validated_data.get("archives", instance.archives)
+        instance.trash     = validated_data.get("trash", instance.trash)
+        instance.pin       = validated_data.get("pin", instance.pin)
         instance.label     = validated_data.get("label", instance.label)
-        labels = label.split(',')
         for label in labels:
-            label = label.strip()
             Label.objects.get_or_create(label_id=user, label=label)
         instance.save()
         return instance
