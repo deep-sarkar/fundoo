e34487a624d6cd3de05f4322393a82d63a376bfd
deep-sarkar <deepsarkar96@gmail.com>
2020-08-06 14:22:00 +0530

    [Deep]Refactor.modified updated collaborators field in create and update methods

--- fundo_project/notes/serializers.py
+++ fundo_project/notes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Label, Note
+from django.contrib.auth.models import User
 
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,7 +9,7 @@ class LabelSerializer(serializers.ModelSerializer):
         read_only_fields = ['id','label_id']
 
 class NoteSerializer(serializers.ModelSerializer):
-    reminder = serializers.TimeField(format="%H:%M", required=False)
+    reminder      = serializers.TimeField(format="%H:%M", required=False)
     
     class Meta:
         model            = Note
@@ -17,11 +18,14 @@ class NoteSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user  = validated_data['user']
+        collaborators = validated_data.pop('collaborators')
         try:
             labels = validated_data['label']
         except KeyError:
             labels = []
         note  = Note.objects.create(**validated_data)
+        note.collaborators.set(collaborators)
+        note.save()
         for label in labels:
             Label.objects.get_or_create(label_id=user, label=label)
         return note
@@ -32,21 +36,23 @@ class SingleNoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model            = Note
-        fields           = ['user','title','note','urls','image','reminder','archives','trash','pin','label']
+        fields           = ['user','title','note','urls','image','reminder','archives','trash','pin','label','collaborators']
         read_only_fields = ['id','user']
 
     def update(self, instance, validated_data):
         user       = validated_data['user']
         labels     = validated_data['label']
-        instance.title     = validated_data.get("title", instance.title)
-        instance.note      = validated_data.get("note", instance.note)
-        instance.urls      = validated_data.get("urls", instance.urls)
-        instance.image     = validated_data.get("image", instance.image)
-        instance.reminder  = validated_data.get("reminder", instance.reminder)
-        instance.archives  = validated_data.get("archives", instance.archives)
-        instance.trash     = validated_data.get("trash", instance.trash)
-        instance.pin       = validated_data.get("pin", instance.pin)
-        instance.label     = validated_data.get("label", instance.label)
+        collaborators = validated_data.pop('collaborators')
+        instance.title         = validated_data.get("title", instance.title)
+        instance.note          = validated_data.get("note", instance.note)
+        instance.urls          = validated_data.get("urls", instance.urls)
+        instance.image         = validated_data.get("image", instance.image)
+        instance.reminder      = validated_data.get("reminder", instance.reminder)
+        instance.archives      = validated_data.get("archives", instance.archives)
+        instance.trash         = validated_data.get("trash", instance.trash)
+        instance.pin           = validated_data.get("pin", instance.pin)
+        instance.label         = validated_data.get("label", instance.label)
+        instance.collaborators.set(collaborators)
         for label in labels:
             Label.objects.get_or_create(label_id=user, label=label)
         instance.save()
