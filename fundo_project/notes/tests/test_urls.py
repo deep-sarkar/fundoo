1d8cfd57e6857a36e4815a73d704a64b696c3a4f
deep-sarkar <deepsarkar96@gmail.com>
2020-08-07 10:51:00 +0530

    [Deep]Add.added test for test open-note url

--- fundo_project/notes/tests/test_urls.py
+++ fundo_project/notes/tests/test_urls.py
@@ -5,3 +5,7 @@ class TestUrls:
     def test_create_note_url(self):
         path = reverse('create-note')
         assert resolve(path).view_name == 'create-note'
+
+    def test_open_note_url(self):
+        path = reverse('open-notes', kwargs={'id':2})
+        assert resolve(path).view_name == 'open-notes'
