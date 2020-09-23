a9c8c8fcc2503ca97250d4cf2e4119b01f2b3e3a
deep-sarkar <deepsarkar96@gmail.com>
2020-09-23 09:23:15 +0530

    [Deep]Refactor.modified refresh jwt to true and default expiry as 3 days

--- fundo_project/fundo_project/settings.py
+++ fundo_project/fundo_project/settings.py
@@ -273,7 +273,8 @@ JWT_AUTH = {
     'JWT_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_payload_handler',
     
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=3),
 }
 
 CACHE_TTL = 60 * 15
