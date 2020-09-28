3d75af29e95b19679c503685ef1f5683e4d0e9b1
deep-sarkar <deepsarkar96@gmail.com>
2020-09-28 14:03:17 +0530

    [Deep]Refactor.modified removed kafka producer from main function to reduce time od=f execution

--- fundo_project/notes/producer.py
+++ fundo_project/notes/producer.py
@@ -2,10 +2,10 @@ from kafka import KafkaProducer
 import json
 import os
 
-clint = os.environ['KAFKA_CLINT']
+client = os.environ['KAFKA_CLIENT']
+producer = KafkaProducer(bootstrap_servers=client)
 
 def add_reminders_to_queue(email,data):
-    producer = KafkaProducer(bootstrap_servers=clint)
     message = "reminder added"
     if len(email)!= 0:
         message ={
